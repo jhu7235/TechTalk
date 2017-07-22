ee9cca57158558e1c508d39cac14553d8fa85291
Jason Hu <jhu7235@github.com>
2017-07-22 11:51:48 -0500

    hide action bar

---
+++ app/src/main/java/awesomeapp/creations/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package awesomeapp.creations.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        actionBar.hide();
+
+//        goToSecondActvity.putExtra();
+    }
+
+
+    public void onNextClick(View view) {
+//        Intent goToSecondActivity = new Intent(this, ScrollingActivity.class);
+//        startActivities();
+    }
+}
